9679e87068e6eda3be2f8b18bb38c3afe4e4bb3f
Ganshyam Suthar <ganshyamsuthar03@gmail.com>
2026-02-08 20:22:55 +0530

    Initial FastAPI todo backend with auth and roles

---
+++ routers/users.py
@@ -0,0 +1,60 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Users
+from .auth import get_current_user, bcrypt_context
+from passlib.context import CryptContext
+
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class UserVerification(BaseModel):
+    current_password: str
+    new_password: str
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def read_user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    
+    user_model = db.query(Users).filter(Users.id==user.get('id')).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user_model
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, 
+                          user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    user_model = db.query(Users).filter(Users.id==user.get('id')).first()
+
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if not bcrypt_context.verify(user_verification.current_password, 
+                                 user_model.hashed_password):
+        raise HTTPException(status_code=400, detail="Current password is incorrect")
+    
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+
