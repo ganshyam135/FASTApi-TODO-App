798ad84bc5fcdb5b42cd3ab8d0a88888724edc93
Ganshyam Suthar <ganshyamsuthar03@gmail.com>
2026-03-02 00:30:44 +0530

    add unit and integration for auth

--- routers/auth.py
+++ routers/auth.py
@@ -62,11 +62,11 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         user_role: str = payload.get('role')
         if username is None or user_id is None:
             raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
-                                detail='Could not vaildate user.')
+                                detail='Could not validate user.')
         return {'username': username, 'id': user_id, 'user_role': user_role}
     except JWTError:
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
-                                detail='Could not vaildate user.')
+                                detail='Could not validate user.')
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
